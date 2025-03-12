a96affb4b2467939465f272ce08e830cb9e58fb2
DDTraining <daniel.ikponmwen@gmail.com>
2025-03-11 18:55:11 -0600

    initialized express backend

---
+++ back-end/src/server.js
@@ -0,0 +1,13 @@
+
+import express from 'express';
+
+const app = express();
+
+app.get('/hello', function (req, res) {
+    res.send('Hello!');
+
+});
+
+app.listen(8000, function () {
+    console.log("the server is listening on port 8000");
+});
